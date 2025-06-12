9a177aa994d0952a56e0750b9320f0b69b8457f6
Sean Rushton <seanpaulrushton@hotmail.com>
2025-06-12 14:39:59 +0100

    CREATE: dockerfile, jenkinsfile plus css and js

--- src/App.js
+++ src/App.js
@@ -35,12 +35,12 @@ function App() {
 
   return (
     <div className='header field'>
-      VAT CALCULATOR
+      Sean's Amazing VAT CALCULATOR
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
-        <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
-        <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
-        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+        <PriceEntryField customstyle="field" label="Price excl VAT £: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
+        <DisplayBlock customstyle="field" label="VAT to pay £: " value={vatToPay} />
+        <PriceEntryField customstyle="field" label="Price incl VAT £: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
       </div>
     </div>
   );
